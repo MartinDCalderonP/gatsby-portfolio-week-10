cca2872edafc103ba9d5c3197700deebbcabf660
Martín Calderón <martincalderon92@outlook.com>
2021-11-18 20:08:45 -0300

    :recycle: Isolate NavbarItem.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,20 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import NavbarItem from "./NavbarItem"
 
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-evenly bg-red-500 p-4 shadow-lg">
       <Link to="/">
-        <button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded">
-          Home
-        </button>
+        <NavbarItem>Home</NavbarItem>
       </Link>
+
       <Link to="/projects">
-        <button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded">
-          Projects
-        </button>
+        <NavbarItem>Projects</NavbarItem>
       </Link>
+
       <Link to="/contact">
-        <button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded">
-          Contact
-        </button>
+        <NavbarItem>Contact</NavbarItem>
       </Link>
     </nav>
   )
---
+++ src/components/NavbarItem.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { INavbarItemProps } from "../common/interfaces"
+
+export default function NavbarItem({ children }: INavbarItemProps) {
+  return (
+    <button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded">
+      {children}
+    </button>
+  )
+}
