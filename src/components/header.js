b27ef910659657fd773b336c241af34796f90550
Martín Calderón <martincalderon92@outlook.com>
2021-11-18 15:26:19 -0300

    💄 Change Header background color.

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#800020`,
+      background: `#db4749`,
       marginBottom: `1.45rem`,
     }}
   >
