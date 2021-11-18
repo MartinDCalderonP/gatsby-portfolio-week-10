b766ccf0dde175e6850fcbfd3baa0e1842fe6737
Martín Calderón <martincalderon92@outlook.com>
2021-11-18 19:00:27 -0300

    :lipstick: Add styles to Footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Footer() {
   return (
-    <footer className="flex items-center justify-evenly bg-red-500 p-4 shadow-lg">
+    <footer className="flex items-center justify-evenly bg-red-500 p-4 shadow-lg absolute bottom-0 w-screen">
       © Martín Calderón {new Date().getFullYear()} - All rights reserved
     </footer>
   )
