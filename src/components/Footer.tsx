f3becf2b457a1004437d8bc36e55e67be15f371f
Martín Calderón <martincalderon92@outlook.com>
2021-11-18 17:41:46 -0300

    :sparkles: Create Footer component.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer>
+      © Martín Calderón {new Date().getFullYear()} - All rights reserved
+    </footer>
+  )
+}
