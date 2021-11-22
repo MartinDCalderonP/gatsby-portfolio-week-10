b781e6685c422f6312fc548118f3dfd5ae08a7ca
Martín Calderón <martincalderon92@outlook.com>
2021-11-22 19:11:07 -0300

    :sparkles: Create 404 page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,12 +2,19 @@ import * as React from "react"
 
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image"
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-black text-4xl py-2">404</h1>
+      <h2 className="text-black text-2xl py-2">Not Found</h2>
+      <p className="text-black py-2">
+        The resource requested could not be found on this server!... maybe I can
+        add this project later. 🤔
+      </p>
+    </div>
   </Layout>
 )
 
