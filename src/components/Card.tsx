672aa03a0783c33469f08631d2e811e01939f439
Martín Calderón <martincalderon92@outlook.com>
2021-11-22 21:29:04 -0300

    📱 Work on responsive design on Card component.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,9 +4,9 @@ import { Link } from "gatsby"
 
 export default function Card({ title, image, slug }: ICardProps) {
   return (
-    <div className="m-4 w-1/3 h-60 rounded-lg border border-black shadow-2xl bg-green-700 text-center">
+    <div className="m-4 w-3/4 lg:w-1/3 h-90 lg:h-60 rounded-lg border border-black shadow-2xl bg-green-700 text-center">
       <Link to={`/project/${slug}`}>
-        <img className="w-full p-2 h-2/3" src={image} alt={title} />
+        <img className="w-full p-2 h-3/4 lg:h-2/3" src={image} alt={title} />
         <h1 className="text-xl p-2">{title}</h1>
       </Link>
     </div>
